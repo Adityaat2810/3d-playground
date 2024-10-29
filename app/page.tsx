4046a440850f1b09ff732c0ee5cc7fa0da388f96
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-10-29 12:54:56 +0530

    helpers

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
 'use client'
 
+import { OrbitControls, Stats } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber'
 import { AnimatedBox } from 'components/AnimatedBox'
 import { CameraOrbitController } from 'components/CameraOrbitController'
 
 export default function Page() {
+  const testing = true;
 
   return (
-    
+
     <main className='h-full w-full'>
       <Canvas>
-        <CameraOrbitController/>
+        {testing ? <Stats /> : null}
+        {testing ? <axesHelper args={[4]} /> : null}
+        {/* grid [size, division] */}
+        {testing ? <gridHelper args={[10,10]} /> : null}
+
+        <OrbitControls />
         <ambientLight intensity={0.1} />
         <directionalLight color="blue" position={[0, 0, 5]} />
-        <AnimatedBox/>
+        <AnimatedBox isTesting={testing} />
       </Canvas>
-      </main>
+    </main>
   )
 }
--- components/AnimatedBox.tsx
+++ components/AnimatedBox.tsx
@@ -1,9 +1,22 @@
+import { useHelper } from "@react-three/drei"
 import { useFrame } from "@react-three/fiber"
 import { useRef } from "react"
+import { BoxHelper } from "three"
 
-export const AnimatedBox = () => {
+type Props={
+    isTesting:boolean
+}
+export const AnimatedBox:React.FC<Props>= ({isTesting}) => {
     const meshRef = useRef(null)  // mesgRef is refering to mesh
+
+    // helper 
+    isTesting ?useHelper(meshRef,BoxHelper,""): null
     /**
+     * Box helper shows the bounding areas
+     */
+
+    /**
+     *  useFrame
      * a hook in React Three Fiber that allows you to run
      *  code on every frame of the render loop
      */
